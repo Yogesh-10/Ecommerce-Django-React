ed9e8aa272468b84ccce7dad850eaeb2deab59db
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-02-10 17:14:16 +0530

    product list screen displayed using redux action

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,8 +2,12 @@ import {
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
+	PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
 
+// get all products
 export const productListReducer = (state = { products: [] }, action) => {
 	switch (action.type) {
 		case PRODUCT_LIST_REQUEST:
@@ -17,6 +21,32 @@ export const productListReducer = (state = { products: [] }, action) => {
 				products: action.payload,
 			}
 		case PRODUCT_LIST_FAIL:
+			return {
+				loading: false,
+				error: action.payload,
+			}
+		default:
+			return state
+	}
+}
+
+// get single product details
+export const productDetailsReducer = (
+	state = { product: { reviews: [] } },
+	action
+) => {
+	switch (action.type) {
+		case PRODUCT_DETAILS_REQUEST:
+			return {
+				loading: true,
+				...state,
+			}
+		case PRODUCT_DETAILS_SUCCESS:
+			return {
+				loading: false,
+				product: action.payload,
+			}
+		case PRODUCT_DETAILS_FAIL:
 			return {
 				loading: false,
 				error: action.payload,
