8dea4fc77dbb999f85db611b0f5a6ffcbe2168a9
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-02-11 07:35:17 +0530

    user token and refresh token added

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,7 +1,46 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import Product
 
+# Serializers allow complex data such as querysets and model instances to be converted to 
+# native Python datatypes that can then be easily rendered into JSON, XML or other content types. 
+# Serializers also provide deserialization, allowing parsed data to be converted back into complex 
+# types, after first validating the incoming data.
+
+class UserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField(read_only=True)
+    _id = serializers.SerializerMethodField(read_only=True)
+    isAdmin = serializers.SerializerMethodField(read_only=True)
+    
+    class Meta:
+        model = User
+        fields =  ['id','_id', 'username', 'email', 'name', 'isAdmin']
+        
+    def get__id(self, obj):
+        return obj.id
+    
+    def get_name(self, obj):
+        name  = obj.first_name
+        if name == '':
+            name = obj.email
+            
+        return name
+    
+    def get_isAdmin(self, obj):
+        return obj.is_staff #these fields(is_staff) comes from default django auth fields , we are customizing it to isAdmin
+        
+
+class UserSerializerWithToken(UserSerializer):
+    token = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = User
+        fields =  ['id','_id', 'username', 'email', 'name', 'isAdmin', 'token']
+        
+    def get_token(self, obj):
+        token = RefreshToken.for_user(obj)
+        return str(token.access_token)
+        
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
--- backend/base/views.py
+++ backend/base/views.py
@@ -5,9 +5,26 @@ from rest_framework.response import Response
 
 from .models import Product
 from .products import products
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+# return username and email in response
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+   def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # data['username'] = self.user.username
+        # data['email'] = self.user.email
+        serializer = UserSerializerWithToken(self.user).data
+        for k, v in serializer.items():
+            data[k] = v
+
+        return data
+            
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -23,4 +40,10 @@ def getProducts(request):
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getUserProfile(request):
+    user = request.user #get user based on jwt token
+    serializer = UserSerializer(user, many=False)
     return Response(serializer.data)
