1c1a59beced9e4942736f9821acdc5788d5d90da
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-02-11 09:54:32 +0530

    signals implemented to overwrite username with email

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 from rest_framework.decorators import api_view, permission_classes 
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Product
 from .products import products
@@ -28,9 +30,6 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
-@api_view(['GET'])
-def getRoutes(request):
-    return Response('Hello')
 
 @api_view(['GET'])
 def getProducts(request):
@@ -56,4 +55,21 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password']),
+        )
+        serializer = UserSerializerWithToken(user, many=False)
+    except:
+        message = {'detail': 'User already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+    
     return Response(serializer.data)
