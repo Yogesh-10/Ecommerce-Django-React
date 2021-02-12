635e782d51beca84c9a09f273a2ec537ed324616
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-02-12 12:29:33 +0530

    Payment method integrated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import CartScreen from './screens/CartScreen'
 import HomeScreen from './screens/HomeScreen'
 import LoginScreen from './screens/LoginScreen'
+import OrderScreen from './screens/OrderScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceorderScreen'
 import ProductScreen from './screens/ProductScreen'
@@ -25,6 +26,7 @@ const App = () => {
 					<Route path='/shipping' component={ShippingScreen} />
 					<Route path='/payment' component={PaymentScreen} />
 					<Route path='/placeorder' component={PlaceOrderScreen} />
+					<Route path='/order/:id' component={OrderScreen} />
 					<Route path='/profile' component={ProfileScreen} />
 					<Route path='/cart/:id?' component={CartScreen} />
 				</Container>
--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -4,6 +4,12 @@ import {
 	ORDER_CREATE_FAIL,
 	ORDER_CREATE_REQUEST,
 	ORDER_CREATE_SUCCESS,
+	ORDER_DETAILS_FAIL,
+	ORDER_DETAILS_REQUEST,
+	ORDER_DETAILS_SUCCESS,
+	ORDER_PAY_FAIL,
+	ORDER_PAY_REQUEST,
+	ORDER_PAY_SUCCESS,
 } from '../constants/orderConstants'
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -30,7 +36,10 @@ export const createOrder = (order) => async (dispatch, getState) => {
 			payload: data,
 		})
 
-		dispatch({ type: CART_CLEAR_ITEMS, payload: data })
+		dispatch({
+			type: CART_CLEAR_ITEMS,
+			payload: data,
+		})
 
 		localStorage.removeItem('cartItems')
 	} catch (error) {
@@ -40,6 +49,84 @@ export const createOrder = (order) => async (dispatch, getState) => {
 				: error.message
 		dispatch({
 			type: ORDER_CREATE_FAIL,
+			payload: message,
+		})
+	}
+}
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+	try {
+		dispatch({
+			type: ORDER_DETAILS_REQUEST,
+		})
+
+		const {
+			userLogin: { userInfo },
+		} = getState()
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		}
+
+		const { data } = await axios.get(`/api/orders/${id}`, config) // api/orders/:id.. in which the id is which that is passed in the paramter above
+
+		dispatch({
+			type: ORDER_DETAILS_SUCCESS,
+			payload: data,
+		})
+	} catch (error) {
+		const message =
+			error.response && error.response.data.detail
+				? error.response.data.detail
+				: error.message
+		dispatch({
+			type: ORDER_DETAILS_FAIL,
+			payload: message,
+		})
+	}
+}
+
+export const payOrder = (orderID, paymentResult) => async (
+	dispatch,
+	getState
+) => {
+	// orderID comes from the database (order we created) and paymentResult from paypal api ..(we can name anything in the parameters we want)
+	// order is whole object, where as objectID is only the id from the whole order object
+	try {
+		dispatch({
+			type: ORDER_PAY_REQUEST,
+		})
+
+		const {
+			userLogin: { userInfo },
+		} = getState()
+
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${userInfo.token}`,
+			},
+		}
+
+		const { data } = await axios.put(
+			`/api/orders/${orderID}/pay`,
+			paymentResult,
+			config
+		)
+
+		dispatch({
+			type: ORDER_PAY_SUCCESS,
+			payload: data,
+		})
+	} catch (error) {
+		const message =
+			error.response && error.response.data.detail
+				? error.response.data.detail
+				: error.message
+		dispatch({
+			type: ORDER_PAY_FAIL,
 			payload: message,
 		})
 	}
