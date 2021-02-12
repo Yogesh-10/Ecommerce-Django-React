f4c6e2c571f96b7ab60e4efd2480e61c16c08cd7
Yogesh <you@example.com>
2021-02-12 21:36:33 +0530

    create product by admin added

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -9,15 +9,59 @@ from base.serializers import ProductSerializer
 
 from rest_framework import status
 
+# get all products
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
 
-
+# get single product by id
 @api_view(['GET'])
 def getProduct(request, pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
+
+# create a product
+@api_view(['POST'])
+@permission_classes([IsAdminUser])
+def createProduct(request):
+    product = Product.objects.create(
+        user = request.user,
+        name='Sample Name',
+        price=0,
+        brand='Sample Brand',
+        countInStock=0,
+        category='Sample category',
+        description=''
+    )
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+# update a product
+@api_view(['PUT'])
+@permission_classes([IsAdminUser])
+def updateProduct(request, pk):
+    data = request.data
+    product = Product.objects.get(_id=pk)
+    
+    product.name = data['name']
+    product.price = data['price']
+    product.brand = data['brand']
+    product.countInStock = data['countInStock']
+    product.category = data['category']
+    product.description = data['description']
+    
+    product.save()
+    
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
+
+# delete a product
+@api_view(['DELETE'])
+@permission_classes([IsAdminUser])
+def deleteProduct(request, pk):
+    product = Product.objects.get(_id=pk)
+    product.delete()
+    return Response('Product Deleted')
